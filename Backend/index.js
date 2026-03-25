cd76cbbdfcbe9bd5f5b2b6c6ea9dfe277777a0f5
Sahil Kamble <syk2448@gmail.com>
2026-03-25 22:21:11 +0530

    add backend folder structure and startup code

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,18 @@ import { addRepo } from "./controllers/add.js";
 import { commitRepo } from "./controllers/commit.js";
 import { pullRepo } from "./controllers/pull.js";
 import { pushRepo } from "./controllers/push.js";
+import express from "express";
 import { revertRepo } from "./controllers/revert.js";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
+dotenv.config();
 yargs(hideBin(process.argv))
+  .command("start", "Start the server", {}, startServer)
   .command("init", "Initialize the new repository", {}, initRepo)
   .command(
     "add <file>",
@@ -52,3 +61,54 @@ yargs(hideBin(process.argv))
   )
   .demandCommand(1, "Please specify a command")
   .help().argv;
+
+const user = "userXYZ";
+function startServer() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  const mongoURI = process.env.MONGO_URI;
+
+  mongoose
+    .connect(mongoURI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => console.log("Error connecting to MongoDB", err));
+
+  app.use(cors({ origin: "*" }));
+  app.get("/", (req, res) => {
+    res.send("Welcome!");
+  });
+
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("joinRoom", (userID) => {
+      user = userID;
+      console.log("=====");
+      console.log(user);
+      console.log("=====");
+      socket.join(userID);
+    });
+  });
+
+  const db = mongoose.connection;
+  db.once("open", async () => {
+    console.log("CRUD OPERATIONS CALLED");
+  });
+
+  const PORT = process.env.PORT || 3000;
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  console.log("Server has started !");
+}
