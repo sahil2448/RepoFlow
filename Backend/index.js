6e7ac16cd3ca4d1b42f1fa0c657a8b654878f96d
Sahil Kamble <syk2448@gmail.com>
2026-06-05 20:02:37 +0530

    add 0.0.0.0

--- Backend/index.js
+++ Backend/index.js
@@ -148,7 +148,7 @@ async function startServer() {
   });
 
   const PORT = process.env.PORT || 3000;
-  httpServer.listen(PORT, () => {
+  httpServer.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
   });
 
