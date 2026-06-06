4ae38573f08f0288d06f2c1681ce4af176053743
Sahil Kamble <syk2448@gmail.com>
2026-06-07 00:58:13 +0530

    add aws s3 connection fix

--- Backend/config/aws-config.js
+++ Backend/config/aws-config.js
@@ -1,5 +1,13 @@
 import AWS from "aws-sdk";
-AWS.config.update({ region: "ap-south-1" });
+import dotenv from "dotenv";
+dotenv.config();
+// AWS.config.update({ region: "ap-south-1" });
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 const s3 = new AWS.S3();
 const S3_BUCKET = "repoflow-bucket";
